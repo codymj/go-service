7b5601a8d6586cdf6f03182e1a66fcfab16b0f75
codymj <codyj@protonmail.com>
2024-09-19 10:20:06 -0500

    revert

--- internal/user/util.go
+++
@@ -1,8 +0,0 @@
-package user
-
-const (
-	InfoBeginGetUserById     = "Begin getting user by ID."
-	InfoDoneGetUserById      = "Done getting user by ID."
-	InfoBeginGetUserByParams = "Begin getting user by parameters."
-	InfoDoneGetUserByParams  = "Done getting user by parameters."
-)
