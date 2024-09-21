18bf71d0d0011b2e4153ec7d035c8801fd41f173
codymj <codyj@protonmail.com>
2024-09-20 19:17:26 -0500

    restructure

--- cmd/app/utility.go
+++ cmd/app/utility.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strconv"
 )
+
+// parseIdParam parses an ID parameter from the requested path.
+func (a *app) parseIdParam(r *http.Request) (int64, error) {
+	parameters := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(parameters.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid ID path parameter")
+	}
+
+	return id, nil
+}
 
 // envelope for wrapping responses.
 type envelope map[string]any
