9ec91cb6d5abe5270f7780acb2adba08bcc28ef4
codymj <codyj@protonmail.com>
2024-09-22 19:42:34 -0500

    add echo

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	e := echo.New()
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "hello, world\n")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
